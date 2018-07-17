1f8e058b9c5a5159765ac488a397ddf534add271
athagar <39389852+athagar@users.noreply.github.com>
2018-07-17 19:45:02 -0400

    enum Faction

---
+++ Faction.cs
@@ -0,0 +1,16 @@
+namespace WarriorWars.Enum
+{
+    enum Faction
+    {
+        GoodGuy,
+        BadGuy,
+        Paladin,
+        Hagre,
+        Bard, 
+        BlackBelt,
+        Assassin,
+        Wizard,
+        Ranger,
+        LizardMan
+    }
+}
